6cb0483e26225edf61cee78796c8a51e24202dec
andyxic <andyxic@163.com>
2016-12-04 15:28:01 +0800

    细节调整

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config:
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <andyxic@163.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or '181551588@qq.com'
+    FLASKY_MAIL_SENDER = 'Flasky Admin'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
@@ -28,19 +28,15 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql+pymysql://root:xichen520@localhost:3306/' \
-                                                                    'hfinter?charset=utf8mb4'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'mysql+pymysql://root:xichen520@localhost:3306/' \
-                                                                     'test?charset=utf8mb4'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql+pymysql://root:xichen520@localhost:3306/' \
-                                                                    'hfinter?charset=utf8mb4'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
 
 
 config = {'development': DevelopmentConfig, 'testing': TestingConfig, 'production': ProductionConfig,
